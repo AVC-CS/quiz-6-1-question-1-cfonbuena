679cf4d1a7a3da2016f53ab8d9d51c4588f5456c
cfonbuena <cfonbuena@students.avc.edu>
2024-10-25 19:31:00 -0700

    add: function prototypes

--- main.hpp
+++ main.hpp
@@ -1,7 +1,11 @@
-
+#include <iostream>
+using namespace std;
 //********************
 // Declare Function Prototypes
 //********************
+void getTwoValues(int, int);
+int getNextPrime(int);
+int getPrevPrime(int);
 
 // ******************************
 // Implement all your functions here
